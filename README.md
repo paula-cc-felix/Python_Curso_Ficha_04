ee49b887292a2b8989517bdf0f9dd88b4c92812f
paula-cc-felix <paula.cc.felix@gmail.com>
2023-06-26 03:14:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python_Curso_Ficha_04
+ Repositório referente à ficha3 do Curso de Python da EISnt
---
+++ ficha04.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 25 11:56:23 2023
+
+@author: paula
+"""
+
+# Objetivo
+# Usar a biblioteca Pandas para analisar um conjunto de dados sobre o consumo de energia
+# elétrica de vários países, e calcular a variação percentual no consumo de energia elétrica em
+# Portugal entre 1990 e 2000
+
+import pandas as pd  #importar a biblioteca Pandas
+
+# Importar os Dados do Excel
+dados =pd.read_excel('consumo_energia_electrica.xlsx', sheet_name='Data')
+
+# Selecionar as colunas de interesse (País, Ano e Consumo de Energia)
+dados = dados[['Country Name', '1990 [YR1990]', '2000 [YR2000]']]
+
+# Filtrar os dados para o Portugal
+dados_portugal = dados[dados['Country Name'] == 'Portugal']
+
+print(dados_portugal)
